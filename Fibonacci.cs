267d2c25aecb69a0b3256393d49a89a8e1ec7c5a
Julita <43172762+JulitaNadolska@users.noreply.github.com>
2023-06-10 09:46:47 +0200

    add solution for generating The Fibonacci sequence

---
+++ Fibonacci.cs
@@ -0,0 +1,47 @@
+using System;
+					
+public class Fibonacci
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the number of objects to be generated: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("The Fibonacci sequence: ");
+
+        for (int i = 0; i < n; i++)
+        {
+            int result = DoFibonacci(i);
+            Console.Write(result + " ");
+        }
+
+        Console.ReadLine();
+    }
+
+    static int DoFibonacci(int n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+        else if (n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            int x = 0;
+            int y = 1;
+            int result = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                result = x + y;
+                x = y;
+                y = result;
+            }
+
+            return result;
+        }
+    }
+}
