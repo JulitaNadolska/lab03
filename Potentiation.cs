994e716bfaf63e0a4e7d6a8bcd41e67cc3d75210
Julita <43172762+JulitaNadolska@users.noreply.github.com>
2023-06-10 09:37:53 +0200

    add solution for potentations of two numbers

---
+++ Potentiation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+					
+public class Potentiation
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the basis: ");
+        int basis = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the exponent: ");
+        int exponent = Convert.ToInt32(Console.ReadLine());
+
+        int result = DoPotentiation(basis, exponent);
+        Console.WriteLine("Result: " + result);
+
+        Console.ReadLine();
+    }
+
+    static int DoPotentiation(int basis, int exponent)
+    {
+        int result = 1;
+
+        for (int i = 0; i < exponent; i++)
+        {
+            result *= basis;
+        }
+
+        return result;
+    }
+}
